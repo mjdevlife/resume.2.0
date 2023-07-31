97150c4c9ba645f6ba5abac94f1617e8110cbf44
Jamal Senior <seniorjamal000@gmail.com>
2023-07-31 14:32:13 +0000

    Install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 function App() {
   return (
       <div>
+        <h1 className='text-red-300'>Welcome to my resume</h1>
       </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(0, 0%, 100%)",
+        secondary: "#202020",
+        tertiary: "#484848",
+        black: "#000000",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontWeight: {
+        hairline: "100",
+        extralight: "200",
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        "extra-bold": "800",
+        black: "900",
+      },
+      minWidth: {
+        rouselMin: "60vw",
+        rousel: "80vw",
+        "home-image": "90vw",
+        "tab-image": "85vw",
+        last: "35vw",
+      },
+      maxWidth: {
+        rousel: "80vw",
+        rouselMin: "60vw",
+        "home-image": "90vw",
+        "tab-image": "85vw",
+        "home-image-low": "40vw",
+        last: "20vw",
+        mid: "50vw",
+      },
+      minHeight: {
+        s: "20vh",
+        sPlus: "40vh",
+        m: "50vh",
+        l: "70vh",
+        xl: "80vh",
+      },
+      maxHeight: {
+        s: "20vh",
+        sPlus: "40vh",
+        m: "50vh",
+        l: "70vh",
+        xl: "80vh",
+      },
+      screens: {
+        // => @media (min-width: 481px) { ... }
+        tablet: "481px",
+
+        // => @media (min-width: 769px) { ... }
+        laptop: "769px",
+
+        // => @media (min-width: 1025px) { ... }
+        desktop: "1025px",
+      },
+    },
+  },
+  plugins: [],
+};
