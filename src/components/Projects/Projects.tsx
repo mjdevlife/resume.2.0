8657354927776c9656e60a533446398491d4dd5a
Jamal Senior <seniorjamal000@gmail.com>
2023-08-01 15:44:45 +0000

    create projects component to structure route

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div className='py-20'>
+        <h1 className='py-20'>THis is a list of projects</h1>
+    </div>
+  )
+}
+
+export default Projects;
