56f6bf294667d5eb2f9bfdf5e29f8687d37b42f8
Jamal Senior <seniorjamal000@gmail.com>
2023-08-02 18:56:25 +0000

    add contact route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar/NavBar";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Projects from "./components/Projects/Projects";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contact from "./components/Contact/Contact";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
