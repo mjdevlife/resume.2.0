0a21086a9c11698a4f8d9fa76f952b173e3dfe2e
Jamal Senior <seniorjamal000@gmail.com>
2023-08-10 17:04:07 +0000

    change returned div to main tag

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,21 @@ import Projects from "./components/Projects/Projects";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from "./components/Contact/Contact";
 import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
 
 const App = () => {
   return (
     <Router>
-      <div>
+      <main>
         <NavBar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
         </Routes>
-      </div>
+      </main>
     </Router>
   );
 };
