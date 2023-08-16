77da5a270bf730c402bc62f376040735b33415b7
Jamal Senior <seniorjamal000@gmail.com>
2023-08-16 19:17:00 +0000

    add experience reducer

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import skillsReducer from './skills/Skills';
 import { createLogger } from "redux-logger";
+import experiencesReducer from './experience/Experience';
 
 const logger = createLogger();
 
 const store = configureStore({
   reducer: {
     skills: skillsReducer,
+    experiences: experiencesReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
