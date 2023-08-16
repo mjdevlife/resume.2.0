bafe25765008fc4c27e7dde79eaa741226a50ee6
Jamal Senior <seniorjamal000@gmail.com>
2023-08-16 19:14:44 +0000

    receive data from redux store to map

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/configureStore';
 
 const Experience = () => {
+  const experiences = useSelector((state: RootState) => state.experiences);
+  
+  const handleModalPop = () => {
+    const [showModal, setShowModal] = useState(false);
+
+  };
+
   return (
     <div className='py-20 px-5 flex flex-col gap-10'>
        <p>
@@ -16,75 +25,21 @@ const Experience = () => {
         <span>Select Comp</span>
        </section>
        <section className='flex flex-col gap-5'>
-         <div className='border-2 flex flex-col items-center gap-2 justify-center px-5 rounded-md py-5'>
-            <span className='text-xl'>Company Name</span>
-            <ul className='flex flex-row gap-2 items-center justify-center border-b-2'>
-              <li>Month, Year Started</li>
-              <li> - </li>
-              <li>Month, Year ended</li>
-            </ul>
-            <span>Tech Stack used</span>
-            <p>
-              In here will be a brief about the company.
-              This will consist of what the company does, where
-              the company is located, why dev felt like the company
-              was the right move, what they worked on, and what they
-              learnt from being at the company.
-            </p>
-            {/* 
-              This button should lead to modal pop where
-              a user can see more about features implemented
-            */}
-            <button type='button' className='border-2 rounded-md p-2 self-end'>
-              View more 
-            </button>
-         </div>
-         <div className='border-2 flex flex-col items-center gap-2 justify-center px-5 rounded-md py-5'>
-            <span className='text-xl'>Company Name</span>
-            <ul className='flex flex-row gap-2 items-center justify-center border-b-2'>
-              <li>Month, Year Started</li>
-              <li> - </li>
-              <li>Month, Year ended</li>
-            </ul>
-            <span>Tech Stack used</span>
-            <p>
-              In here will be a brief about the company.
-              This will consist of what the company does, where
-              the company is located, why dev felt like the company
-              was the right move, what they worked on, and what they
-              learnt from being at the company.
-            </p>
-            {/* 
-              This button should lead to modal pop where
-              a user can see more about features implemented
-            */}
-            <button type='button' className='border-2 rounded-md p-2 self-end'>
-              View more 
-            </button>
-         </div>
-         <div className='border-2 flex flex-col items-center gap-2 justify-center px-5 rounded-md py-5'>
-            <span className='text-xl'>Company Name</span>
-            <ul className='flex flex-row gap-2 items-center justify-center border-b-2'>
-              <li>Month, Year Started</li>
-              <li> - </li>
-              <li>Month, Year ended</li>
-            </ul>
-            <span>Tech Stack used</span>
-            <p>
-              In here will be a brief about the company.
-              This will consist of what the company does, where
-              the company is located, why dev felt like the company
-              was the right move, what they worked on, and what they
-              learnt from being at the company.
-            </p>
-            {/* 
-              This button should lead to modal pop where
-              a user can see more about features implemented
-            */}
-            <button type='button' className='border-2 rounded-md p-2 self-end'>
-              View more 
-            </button>
-         </div>
+          {experiences.map((experience) => (
+            <div key={experience.id} className='border-2 flex flex-col items-center gap-2 justify-center px-5 rounded-md py-5'>
+              <span className='text-xl'>{experience.companyName.toUpperCase()}, <i className='text-sm'>Remote</i></span>
+              <ul className='flex flex-row gap-2 items-center justify-center border-b-2'>
+                <li>{experience.employmentSpan[0].toUpperCase()}</li>
+                <li> - </li>
+                <li>{experience.employmentSpan[1].toUpperCase()}</li>
+              </ul>
+              <span className='underline'>{experience.employmentStack}</span>
+              <p>{experience.projectBrief[0].slice(0, 300)}...</p>
+              <button type='button' className='border-2 rounded-md p-2 self-end' onClick={() => handleModalPop()}>
+                View more 
+              </button>
+            </div>
+          ))}
        </section>
     </div>
   )
