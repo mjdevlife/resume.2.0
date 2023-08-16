89cceee519ce3f820c2f0e864ae32f2d9672c740
Jamal Senior <seniorjamal000@gmail.com>
2023-08-16 19:16:15 +0000

    add experience/actions file

---
+++ src/components/Experience/ModalPop.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ModalPop = () => {
+  return (
+    <div>
+        <button>
+            Close
+        </button>
+    </div>
+  )
+}
+
+export default ModalPop;
