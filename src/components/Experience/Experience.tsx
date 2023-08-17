9ca4e9f85baae0aaf338ab8eaac41376433cc4e9
Jamal Senior <seniorjamal000@gmail.com>
2023-08-17 18:29:39 +0000

    add modalPop and options for react-select

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,14 +1,42 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/configureStore';
+import ModalPop from './ModalPop';
+import { ExperienceData } from '../../redux/experience/types';
+import Select from 'react-select';
 
 const Experience = () => {
   const experiences = useSelector((state: RootState) => state.experiences);
-  
-  const handleModalPop = () => {
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [employment, setSelectedEmployment] = useState<ExperienceData | null>(null);
 
+  // Use a Set to ensure unique values
+  const uniqueOptionsVal = new Set<string>();
+
+  experiences.forEach((exp) => {
+    exp.searchStack.forEach((stack) => {
+      uniqueOptionsVal.add(stack);
+    });
+  });
+
+  const handleModalPop = (selectedEmployment: ExperienceData) => {
+    setShowModal(true);
+    setSelectedEmployment(selectedEmployment)
   };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    setSelectedEmployment(null);
+  };
+
+  // Convert uniqueOptionsVal Set to an array
+  const optionsVal = Array.from(uniqueOptionsVal);
+
+  // Construct options array from optionsVal
+  const options = [
+    { value: '', label: 'All' },
+    ...optionsVal.map((stack) => ({ value: stack, label: stack })),
+  ];
 
   return (
     <div className='py-20 px-5 flex flex-col gap-10'>
@@ -20,9 +48,13 @@ const Experience = () => {
           new ways to leverage emerging tools and frameworks to create innovative solutions.
           Below are companies I have worked with.
        </p>
-       <section className='flex flex-row gap-5 justify-end'>
+       <section className='flex flex-row gap-5 justify-end items-center'>
         <span>Sort by stack:</span>
-        <span>Select Comp</span>
+        <Select
+            options={options}
+            defaultValue={[options[0]]}
+            className='w-[50vw]'
+        />
        </section>
        <section className='flex flex-col gap-5'>
           {experiences.map((experience) => (
@@ -35,11 +67,12 @@ const Experience = () => {
               </ul>
               <span className='underline'>{experience.employmentStack}</span>
               <p>{experience.projectBrief[0].slice(0, 300)}...</p>
-              <button type='button' className='border-2 rounded-md p-2 self-end' onClick={() => handleModalPop()}>
+              <button type='button' className='border-2 rounded-md p-2 self-end' onClick={() => handleModalPop(experience)}>
                 View more 
               </button>
             </div>
           ))}
+          {showModal && <ModalPop selectedEmployment={employment} handleModalClose={handleModalClose} />}
        </section>
     </div>
   )
