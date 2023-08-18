82d94466f6b29098d4030800ac1513cefd8b3c9b
Jamal Senior <seniorjamal000@gmail.com>
2023-08-18 17:37:58 +0000

    add logic to filter experience by searchStack

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -9,6 +9,7 @@ const Experience = () => {
   const experiences = useSelector((state: RootState) => state.experiences);
   const [showModal, setShowModal] = useState(false);
   const [employment, setSelectedEmployment] = useState<ExperienceData | null>(null);
+  const [selectedOption, setSelectedOption] = useState<string>("");
 
   // Use a Set to ensure unique values
   const uniqueOptionsVal = new Set<string>();
@@ -39,9 +40,21 @@ const Experience = () => {
     ...optionsVal.map((stack) => ({ value: stack, label: stack })),
   ];
 
+  const filteredExperiences = experiences.filter((exp: { searchStack: string[]}) => {
+    if (selectedOption === "") {
+      return true;
+    } else {
+      return exp.searchStack.includes(selectedOption)
+    }
+  });
+
+  const handleSelectChange = (e: any) => {
+    setSelectedOption(e.value);
+  };
+
   return (
-    <div className='py-20 px-5 flex flex-col gap-10'>
-       <p>
+    <div className='py-20 px-5 flex flex-col gap-10 tablet:px-[8vw] desktop:px-[12vw]'>
+       <p className='desktop:text-2xl'>
           My expertise lies in programming and implementing web-based solutions that
           meet the evolving needs of modern businesses. I have over 8 years of experience in a wide range of technologies,
           including Typescript, JavaScript, React, Ruby on Rails, AWS, GraphQL, and Docker.
@@ -49,24 +62,32 @@ const Experience = () => {
           new ways to leverage emerging tools and frameworks to create innovative solutions.
           Below are companies I have worked with.
        </p>
-       <section className='flex flex-row gap-5 justify-end items-center'>
-        <span>Sort by stack:</span>
+       <section className='flex flex-row gap-5 justify-center items-center tablet:px-10 desktop:px-0'>
+        <ul className='inline-flex flex-row flex-wrap'>
+          <li className='pr-1'>Sort by</li>
+          <li>stack / language / tool:</li>
+        </ul>
         <Select
             options={options}
             defaultValue={[options[0]]}
-            className='w-[50vw]'
+            className='w-[50vw] tablet:w-[70vw] desktop:w-[28vw]'
+            onChange={handleSelectChange}
         />
        </section>
-       <section className='flex flex-col gap-5'>
-          {experiences.map((experience) => (
-            <div key={experience.id} className='border-2 flex flex-col items-center gap-2 justify-center px-5 rounded-md py-5'>
-              <span className='text-xl'>{experience.companyName.toUpperCase()}, <i className='text-sm'>Remote</i></span>
-              <ul className='flex flex-row gap-2 items-center justify-center border-b-2'>
+       <section className='flex flex-col gap-5 tablet:px-[15vw] desktop:flex-row flex-wrap'>
+          {filteredExperiences.map((experience) => (
+            <div key={experience.id}
+              className='border-2 flex flex-col items-center gap-2 justify-center px-5 rounded-md py-5 desktop:w-[22vw]'
+            >
+              <span className='text-xl tablet:text-3xl'>{experience.companyName.toUpperCase()},
+                <i className='text-sm tablet:text-lg'>Remote</i>
+              </span>
+              <ul className='flex flex-row gap-2 items-center justify-center border-b-2 tablet:text-xl desktop:text-xs'>
                 <li>{experience.employmentSpan[0].toUpperCase()}</li>
                 <li> - </li>
                 <li>{experience.employmentSpan[1].toUpperCase()}</li>
               </ul>
-              <span className='underline'>{experience.employmentStack}</span>
+              <span className='underline tablet:text-xl text-center'>{experience.employmentStack}</span>
               <p>{experience.projectBrief[0].slice(0, 300)}...</p>
               <button type='button' className='border-2 rounded-md p-2 self-end' onClick={() => handleModalPop(experience)}>
                 View more 
